4a235ce36463156e672c2875f689c8e79b0d7447
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-06-19 11:16:51 +0800

    Initial Commit

---
+++ TimeStateContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace BlazorWasmBarebone
+{
+    public static class TimeStateContainer
+    {
+        private static readonly Stopwatch Stopwatch = new ();
+
+        public static void Start() => Stopwatch.Start();
+
+        public static long Stop()
+        {
+            Stopwatch.Stop();
+            return Stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
